5db57a51dcf41409eeba1e157d858b2e99fc9f34
sagar <sagar4504@rediffmail.com>
2022-01-25 21:45:43 +0530

    modified

--- src/main/java/lockedMe/LockedMe.java
+++ src/main/java/lockedMe/LockedMe.java
@@ -1,21 +1,24 @@
 package lockedMe;
 
+import java.io.File;
+import java.util.LinkedList;
 import java.util.Scanner;
-
+import java.io.*;
 public class LockedMe {
 	
-
+static final  String projectFilesPath ="C:\\Users\\Admin\\Desktop\\LockedMeProjewc\\LockedMeFile";
+static final String errorMessage="some error occured please contact admin:";
 	public static void main(String[] args)
-	{
-		displayWelcome();
+{
+		displayWelcome();		
 		Scanner obj = new Scanner(System.in);
+		try {
 		int ch;
-		do {
-			displayMenu();
-			ch=Integer.parseInt(obj.nextLine());
-			
+			do	{
+				displayMenu();
+				ch=Integer.parseInt(obj.nextLine());
 			switch(ch)
-			{
+				{
 			case 1: getAllFiles();
 				break;
 			case 2:creatFiles();
@@ -31,39 +34,136 @@ public class LockedMe {
 			break;
 			default: System.out.println("Invalid Option ");
 			break;
+				}
 			}
-		}
 		while(ch>0);
+		}
+		catch(Exception ex)
+		{
+				System.out.println(errorMessage);
+		}
 		obj.close();
-	}
-	
+}
 public static void displayWelcome()
 {
 	System.out.println("====================================================");
 	System.out.println("\t\tWelcome to Lockedme.com ");
 	System.out.println("====================================================");
-	System.out.println("\t\tDeveloped by Mr.Sagar Godambbe");
+	System.out.println("\t\t Developed by Mr.Sagar Godambbe ");
 	System.out.println("----------------------------------------------------");
 }
 public static void displayMenu()
-{System.out.println("Enter Your Choice: ");
+{
+System.out.println("Enter Your Choice: ");
 System.out.println("\t\t1:Display all the Files:");
 System.out.println("\t\t2:Add a New file:");
 System.out.println("\t\t3:Delete a File:");
 System.out.println("\t\t4:Search a File:");
 System.out.println("\t\t5:Exit");
 }
-
+/**-----------------------------------------------------------------
+ * this function will return  all the file from project directory.
+ */
 public static void getAllFiles()
 {
+	try {
+	File folder = new File(projectFilesPath);
+	File[] listofFiles = folder.listFiles();
+	
+	if(listofFiles.length==0)
+		System.out.println("No Files Exist in the directory");
+	else	{
+		for (var l:listofFiles)
+		    {
+			System.out.println(l.getName());
+		    }
+	}
 }
+	catch(Exception ex) {
+		System.out.println(errorMessage);
+			}
+	}
+/**-------------------------------------------------------------------
+ *  * this function will create the file in project directory
+
+ */
 public static void creatFiles()
 {	
+	try  {
+		Scanner obj=new Scanner (System.in);
+		String filename;
+		System.out.println("Enter file name:  ");
+		filename=obj.nextLine();
+		
+		int linesCount;
+		System.out.println("Enter how many lines in the Files: ");
+		linesCount=Integer.parseInt(obj.nextLine());
+		
+		FileWriter myWriter = new FileWriter(projectFilesPath+"\\"+filename);
+	for (int i=1;i<=linesCount;i++)
+	{
+		System.out.println("Enter the File Line: ");
+		myWriter.write(obj.nextLine()+"\n");
+	}
+	System.out.println("File Created Successfully !");
+	}
+	catch(Exception Ex)
+	{System.out.println(errorMessage);
+	}
 }
+/**--------------------------------------------------------------------------------
+ *  * this function will delete  the file from project directory
+
+ */
 public static void deleteFiles()
 {
+	Scanner obj= new Scanner(System.in);
+	try
+	{
+		String fileName;
+	System.out.println("Enter the File Name to be deleted:");
+		fileName =obj.nextLine();
+	File file=new File(projectFilesPath+"\\"+fileName);
+	if  (file.exists())
+		{
+	file.delete();
+	System.out.println("File Deleted Successfully! ");
+		}
+else
+	System.out.println("File Do not exist!");
+	}
+	catch (Exception Ex)
+	{
+	System.out.println(errorMessage);
+	}
 }
+/**------------------------------------------------------------------------------------
+ *  * this function will search   the file from project directory
+
+ */
 public static void searchFiles()
 {
-}
+	Scanner obj=new Scanner(System.in);
+	try	{
+		System.out.println("Enter the FileName tobe  Searched :");
+		String	fileName =obj.nextLine();
+		
+		File folder =new File(projectFilesPath);
+		File[] listofFiles=folder.listFiles();
+		
+		LinkedList<String> filenames=new LinkedList<String>();
+		
+		for (var l:listofFiles)
+			filenames.add(l.getName());
+		
+		if (filenames.contains(fileName))
+			System.out.println("File is Availbale!");
+		else
+			System.out.println("File is not Available!");
+		}
+	catch (Exception Ex)
+	{
+	System.out.println(errorMessage);
+	}
+	}
 }
